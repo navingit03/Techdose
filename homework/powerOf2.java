f6cc0dcf0d5e0f25f26d7a58ab2c81d2b1976db0
navingit03 <navinvenkatesh.k2020ece@sece.ac.in>
2022-08-27 22:49:26 +0530

    powerOf2

---
+++ homework/powerOf2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Java{
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print((n&n-1)==0);
+    }
+}
