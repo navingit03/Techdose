80d6e11c01aa18b5b09b28dfb020602e49df21a6
navingit03 <navinvenkatesh.k2020ece@sece.ac.in>
2022-09-03 22:27:23 +0530

    aPowerb

---
+++ Day4NumberTheory/aPowerb.java
@@ -0,0 +1,20 @@
+package Day4NumberTheory;
+
+import java.util.Scanner;
+
+public class aPowerb {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int res=1;
+        while(b>0){
+            if(b%2==1){
+                res*=a;
+            }
+            a=(a*a);
+            b/=2;
+        }
+        System.out.print("a power of b is " + res);
+    }
+}
