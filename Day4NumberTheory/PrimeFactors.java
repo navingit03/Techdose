52cb4f540aeec259d13d739ece83addf90c1f425
navingit03 <navinvenkatesh.k2020ece@sece.ac.in>
2022-09-03 17:59:25 +0530

    PrimeFactorsOfn

---
+++ Day4NumberTheory/PrimeFactors.java
@@ -0,0 +1,18 @@
+package Day4NumberTheory;
+import java.util.*;
+public class PrimeFactors {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        for(int i=2;i*i<=N;i++){
+            while((N%i)==0){
+                System.out.print(i+" ");
+                N/=i;
+            }
+            
+        }
+        if(N>1){
+            System.out.print(N+" ");
+        }
+    }
+}
