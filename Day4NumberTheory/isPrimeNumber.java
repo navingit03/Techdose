c4587d7a066c84b924a647cd9f3099e2b77a4a4f
navingit03 <navinvenkatesh.k2020ece@sece.ac.in>
2022-09-03 15:34:18 +0530

    isPrimeNumber

---
+++ Day4NumberTheory/isPrimeNumber.java
@@ -0,0 +1,15 @@
+package Day4NumberTheory;
+import java.util.*;
+public class isPrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                System.out.print("N is not a prime number");
+                System.exit(0);
+            }
+        }
+        System.out.print("N is a prime number");
+    }
+}
