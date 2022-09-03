8b5eb5350915b063c5a92277244a689701c382ba
navingit03 <navinvenkatesh.k2020ece@sece.ac.in>
2022-09-03 20:39:25 +0530

    PrimesBetween1toN

---
+++ Day4NumberTheory/PrimesBetween1toN.java
@@ -0,0 +1,23 @@
+package Day4NumberTheory;
+import java.util.*;
+public class PrimesBetween1toN {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        boolean arr[]=new boolean[n+1];
+        Arrays.fill(arr,true);
+        for(int i=2;i*i<=n;i++){
+            if(arr[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    arr[j]=false;
+                }
+            }
+        }
+        for(int i=2;i<=n;i++){
+            if(arr[i]){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
