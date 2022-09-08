cb408e7462bd452342e03eef6a8a75cfb42d0d34
navingit03 <navinvenkatesh.k2020ece@sece.ac.in>
2022-09-08 22:12:32 +0530

    BubbleSort

---
+++ SortingAlgorithm/BubbleSort.java
@@ -0,0 +1,36 @@
+package SortingAlgorithm;
+
+import java.util.Scanner;
+public class BubbleSort {
+    public int[] BubbleSortAlgo(int arr[],int size){
+        for(int i=0;i<size-1;i++){
+            boolean isSwapped=false;
+            for(int j=0;j<size-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    arr[j]=arr[j]^arr[j+1];
+                    arr[j+1]=arr[j]^arr[j+1];
+                    arr[j]=arr[j]^arr[j+1];
+
+                    isSwapped=true;
+                }
+            }
+            if(!isSwapped){
+                break;
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        BubbleSort obj=new BubbleSort();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        obj.BubbleSortAlgo(arr,n);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
