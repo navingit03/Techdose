c151b5132b0e1fb474f9ba0d53c6e3bd167963c5
navingit03 <navinvenkatesh24@gmail.com>
2022-09-11 21:24:06 +0530

    Assignment

---
+++ BeginnerAssignment/AddIntegerToDecimal.java
@@ -0,0 +1,12 @@
+package BeginnerAssignment;
+
+import java.util.Scanner;
+
+public class AddIntegerToDecimal {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        double d=sc.nextDouble();
+        System.out.print(i+d);
+    }
+}
