ef688593f89ef0003291ce0426d89f94bc35b480
navingit03 <navinvenkatesh24@gmail.com>
2022-09-11 17:09:57 +0530

    Assignment

---
+++ BeginnerAssignment/AreaOfTriangle.java
@@ -0,0 +1,12 @@
+package BeginnerAssignment;
+import java.util.*;
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x1=sc.nextInt(),y1=sc.nextInt();
+        int x2=sc.nextInt(),y2=sc.nextInt();
+        int x3=sc.nextInt(),y3=sc.nextInt();
+        double area=(0.5)*((x1*(y2-y3))+(x2*(y3-y1))+(x3*(y1-y2)));
+        System.out.print(area);
+    }    
+}
