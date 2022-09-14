b347904c6d3c02715fd95e5ff5a84506c888814c
navingit03 <navinvenkatesh24@gmail.com>
2022-09-14 19:37:43 +0530

    Assignment

---
+++ BeginnerAssignment/PrintDigitsOfN.java
@@ -0,0 +1,18 @@
+package BeginnerAssignment;
+import java.util.Scanner;
+public class PrintDigitsOfN {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int digit[]=new int[10];
+        int index=0;
+        while(N>0){
+            digit[index]=(N%10);
+            index++;
+            N/=10;
+        }
+        for(int i=index-1;i>=0;i--){
+            System.out.println(digit[i]);
+        }
+    }
+}
