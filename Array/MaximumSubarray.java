586047d3e76dc587b47a1dcb6ece5ab732d55fd4
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-09-23 22:52:39 +0530

    Assignment

---
+++ Array/MaximumSubarray.java
@@ -0,0 +1,15 @@
+//leetcode 53. Maximum Subarray(Largest sum contiguous subarray)
+//Kadane's algorithm
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=nums[0],sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>max)
+                max=sum;
+            if(sum<0)
+                sum=0;
+        }
+        return max;
+    }
+}
