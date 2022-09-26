5b4e805da0431bb88f69b066486657fc71439814
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-09-27 00:04:07 +0530

    Assignment

--- Array/TrappingRainWater.java
+++ Array/TrappingRainWater.java
@@ -12,19 +12,22 @@ class Solution {
         int trappedWater=0;
         int left=1,right=n-2;
         while(left<=right){
-            if(maxLeft<maxRight){
-                if(height[left]>maxLeft){
-                    maxLeft=height[left];
+            if(maxLeft<maxRight) //if maxRight is high
+            {
+                if(height[left]>maxLeft)
+                {
+                    maxLeft=height[left]; // if current height greather than maxLeft
                 }else{
-                    trappedWater+=(maxLeft-height[left]);
+                    trappedWater+=(maxLeft-height[left]); //else adding the min(maxLeft,maxRight) - height[left]
                 }
                 left++;
             }
-            else{
+            else //if maxLeft is high
+            {
                 if(height[right]>maxRight){
-                    maxRight=height[right];
+                    maxRight=height[right]; // if current height greather than maxRigth
                 }else{
-                    trappedWater+=(maxRight-height[right]);
+                    trappedWater+=(maxRight-height[right]); //else adding the min(maxLeft,maxRight) - height[left]
                 }
                 right--;
             }
