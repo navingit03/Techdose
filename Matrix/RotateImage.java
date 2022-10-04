3c2c27598fe4af675a8ee3194c0fdd3d48283f68
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-10-04 10:34:04 +0530

    Assignment

---
+++ Matrix/RotateImage.java
@@ -0,0 +1,22 @@
+//Leetcode 48. Rotate Image
+//Taking transpose and reversing row-wise
+class Solution {
+    public void rotate(int[][] matrix) {
+        //Transpose of the matrix
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[i].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+      //Reversing row-wise
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix[i].length-j-1];
+                matrix[i][matrix[i].length-j-1]=temp;
+            }
+        }
+    }
+}
