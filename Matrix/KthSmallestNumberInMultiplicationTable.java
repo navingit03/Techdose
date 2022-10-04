49f87ba2da9991de06d4a717fd1e5b84ec1f6c50
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-10-04 18:21:48 +0530

    Assignment

---
+++ Matrix/KthSmallestNumberInMultiplicationTable.java
@@ -0,0 +1,28 @@
+//Leetcode 668. Kth Smallest Number in Multiplication Table
+//Binary Search
+class Solution {
+    public int lessthanK(int mid,int m,int n) //function used to find number of elements which is <= mid between low and high
+    {
+        int sum=0;
+        for(int i=1;i<=m;i++){
+            sum+=Math.min(mid/i,n); minimum of mid/row , col
+        }
+        return sum; //no of elements <= mid
+    }
+    public int findKthNumber(int m, int n, int k) {
+        int low=1; //initially low = 1
+        int high=m*n; //initially high = m*n
+        while(low<=high){
+            int mid=low+(high-low)/2; //finding the middle element
+            if(lessthanK(mid,m,n)<k) // If no of elements from 1 to mid is less than k, then kth smallest element will be for mid to m*n so updating low = mid+1
+            {
+                low=mid+1;
+            }
+            else //high=mid-1
+            {
+                high=mid-1;
+            }
+        }
+        return low; //returning the k th smallest element 
+    }
+}
