2f4fecc8e1da8c6bc3beb9e2d33eea6a230c7fa1
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-10-10 10:52:53 +0530

    Assignment

--- Array/CountInversion.java
+++ Array/CountInversion.java
@@ -4,7 +4,8 @@ class HelloWorld {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int arr[]=new int[n];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             arr[i]=sc.nextInt();
         }
         int countInversion=0;
