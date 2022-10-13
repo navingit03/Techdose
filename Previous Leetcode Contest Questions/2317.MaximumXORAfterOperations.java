36af2800831d484467bb1b8de9f970cdd1632c73
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-10-13 11:42:11 +0530

    Assignment

--- Previous Leetcode Contest Questions/2317.MaximumXORAfterOperations.java
+++ Previous Leetcode Contest Questions/2317.MaximumXORAfterOperations.java
@@ -19,3 +19,4 @@ Explanation: Apply the operation with x = 4 and i = 3, num[3] = 6 AND (6 XOR 4) 
 Now, nums = [3, 2, 4, 2] and the bitwise XOR of all the elements = 3 XOR 2 XOR 4 XOR 2 = 7.
 It can be shown that 7 is the maximum possible bitwise XOR.
 Note that other operations may be used to achieve a bitwise XOR of 7.
+*/
