60e0f5ecae90896d3b87e6da772367f3e7cdfc98
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-10-19 15:27:01 +0530

    Assignment

---
+++ LinkedList/ReverseNodesInK-Group.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy=new ListNode(0,head);
+        ListNode ptr=dummy;
+        while(ptr!=null){
+            ListNode temp=ptr;
+            for(int i=0;i<k&&temp!=null;i++){
+                temp=temp.next;
+            }
+            if(temp==null) break;
+            ListNode prev=null,curr=ptr.next,next=ptr.next.next;
+            for(int i=0;i<k;i++){
+                next=curr.next;
+                curr.next=prev;
+                prev=curr;
+                curr=next;
+            }
+            ListNode tail=ptr.next;
+            tail.next=curr;
+            ptr.next=prev;
+            ptr=tail;
+        }
+        return dummy.next;
+    }
+}
