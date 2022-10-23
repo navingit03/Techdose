1da8ca6ec8c98f301005fa17210b35d6660dfb43
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-10-23 12:32:17 +0530

    Assignment

--- LeetcodeContest(1stQuestion)/392.IsSubsequence.java
+++ LeetcodeContest(1stQuestion)/392.IsSubsequence.java
@@ -8,3 +8,14 @@ class Solution {
         return j==s.length(); //if all char in s is found in t return true
     }
 }
+/*
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+*/
