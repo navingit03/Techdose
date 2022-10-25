93b2f6cc9d88de09642259f69f8ef7818bd76600
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-10-25 23:26:56 +0530

    Assignment

---
+++ Stack&Queue/Assignments/NextGreaterElement.java
@@ -0,0 +1,30 @@
+package StackQueue;
+import java.util.*;
+public class nextGreaterElement {
+    public static int[] NGE(int[] arr){
+        Stack<Integer> stack=new Stack<>(); //stack to add index
+        int res[]=new int[arr.length]; //creating res array for NGE
+        for(int i=arr.length-1;i>=0;i--){
+            while(!stack.isEmpty()&&(arr[stack.peek()]<=arr[i])) //finding next small element from last
+            {
+                stack.pop();
+            }
+            res[i]=stack.isEmpty()?-1:arr[stack.peek()]; //if not empty then top is NGE
+            stack.push(i);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Original array : "+Arrays.toString(arr));
+        sc.close();
+        int []res=NGE(arr); //sending the original array to find next greater element
+        System.out.print("Next Greater element Array : "+Arrays.toString(res));
+
+    }
+}
