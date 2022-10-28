5db6ee568c5d9e1532c6d776e6f75a52e82e9ba2
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-10-28 14:41:49 +0530

    Assignment

--- Heap/Algorithms/DecreaseKeyInMinHeap.java
+++ Heap/Algorithms/DecreaseKeyInMinHeap.java
@@ -7,13 +7,14 @@ public class DecreaseKeyInMinHeap {
         System.out.println("Original Array before decrease key : "+Arrays.toString(arr));
         int updateIndex=4; //input index where we have to update the value at particular index
         int updateDiff=-8; //update index value by diff
-        arr[updateIndex]+=updateDiff;
+        arr[updateIndex]+=updateDiff; //updating the value at index
         int i=updateIndex;
-        while(i>0 && arr[i]<arr[i/2-1]){
-            int temp=arr[i];
+        while(i>0 && arr[i]<arr[i/2-1]) //index i should below top(min element) else underflow happen as i/2 not avaiable
+        {
+            int temp=arr[i]; //if(child is smaller than its parent(i/2) we need to swap parent and child)
             arr[i]=arr[i/2-1];
             arr[i/2-1]=temp;
-            i/=2;
+            i/=2; //moving to curr index's parent. i should not be <=0 as top 0 does not have parent
         }
         System.out.println("MinHeap Array after decrease value at index : "+Arrays.toString(arr));
         sc.close();
