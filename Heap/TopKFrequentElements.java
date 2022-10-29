9d09c241ab8b52d5898d15b8c40d44f4289c6f92
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-10-29 11:31:26 +0530

    Assignment

---
+++ Heap/TopKFrequentElements.java
@@ -0,0 +1,32 @@
+//Leetcode 347. Top K Frequent Elements
+//return k elements(these k elements will have high frequency than other elements in nums)
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> freq=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1); //frequency of nums[i] in hashmap
+        }
+        PriorityQueue<Integer> maxfreqHeap=new PriorityQueue<>((a,b)-> freq.get(b)-freq.get(a)); //pq is arranged based on most freq element
+        for(int element:freq.keySet()) //adding key of hashmap in heap(it will be arraged as high freq element at top of heap tree as (a,b)-> freq.get(b)-freq.get(a))
+        {
+            maxfreqHeap.add(element); //to add all key we also use : maxfreqHeap.addAll(freq.keySet());
+        }
+        int res[]=new int[k]; //output most freq k elements
+        for(int i=0;i<k;i++)
+        {
+            res[i]=maxfreqHeap.poll(); //polling all high freq element
+        }
+        return res;
+    }
+}
+/*
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+*/
