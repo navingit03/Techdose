35c2ae8d1e51d7a1a8dc27eb87fecfb3244d7aeb
navingit03 <112114128+navingit03@users.noreply.github.com>
2022-11-05 12:38:49 +0530

    Assignment

---
+++ Recursion/CombinationSum.java
@@ -0,0 +1,29 @@
+//Leetcode 39. Combination Sum
+class Solution {
+    List<List<Integer>> result;
+    public void dfs(int []arr,int target,int index,int sum,List<Integer> temp){
+        if(target==sum) //if sum==target
+        {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        if(target<sum) //if sum > target we need to go back
+        {
+            return;
+        }
+        if(index==arr.length) //end of the array
+        {
+            return;
+        }
+        temp.add(arr[index]); //adding current array[index]
+        backtrack(arr,target,index,sum+arr[index],temp); //include
+        temp.remove(temp.size()-1); //removing current array[index]
+        backtrack(arr,target,index+1,sum,temp); //exclude
+        
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        result=new ArrayList<>();
+        dfs(candidates,target,0,0,new ArrayList<>()); //dfs method call
+        return result;
+    }
+}
