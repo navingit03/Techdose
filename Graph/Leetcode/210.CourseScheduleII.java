646742ef1c6ff4e7864c336a0955fe5986fb61c8
Navin Venkatesh K <112114128+navingit03@users.noreply.github.com>
2023-01-07 23:21:48 +0530

    Assignment

--- Graph/Leetcode/210.CourseScheduleII.java
+++ Graph/Leetcode/210.CourseScheduleII.java
@@ -1,40 +1,66 @@
 //210. Course Schedule II using topological sort 
 //if cycle found return empty arr
 class Solution {
-    //dfs three color in directed graph to find cycle
-    public static boolean dfs(List<List<Integer>> adjList,int visited[],int curr){
-        if(visited[curr]==2)//already processed
+    int index; //to store ans at correct index of array
+    public boolean isCycle(List<List<Integer>> adjList,int []color,int curr) //if cycle is present or not
+    {
+        if(color[curr]==2)//already processed
         {
             return false;
         }
-        if(visited[curr]==1) //if its already visited of curr dfs call
+        if(color[curr]==1) //if its already visited of curr dfs call
         {
             return true;
         }
-        visited[curr]=1;
+        color[curr]=1;
         for(int i=0;i<adjList.get(curr).size();i++){
             int adj=adjList.get(curr).get(i);
-            if(dfs(adjList, visited, adj)==true){
+            if(isCycle(adjList,color,adj)==true){
                 return true;
             }
         }
-        visited[curr]=2;//processed
+        color[curr]=2; //making it processed
         return false;
     }
-    public boolean canFinish(int n, int[][] p) {
-        List<List<Integer>> list=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            list.add(new ArrayList<>());
+    public void dfs(List<List<Integer>> adjList,boolean []visited,int curr,int []ans){
+        if(visited[curr]){
+            return;
         }
-        for(int i=0;i<p.length;i++){
-            list.get(p[i][0]).add(p[i][1]);
-        }
-        int visited[]=new int[n];
-        for(int i=0;i<n;i++){
-            if(dfs(list,visited,i)){
-                return false;
+        visited[curr]=true;
+        for(int i=0;i<adjList.get(curr).size();i++){
+            int adj=adjList.get(curr).get(i);
+            if(!visited[adj]){
+                dfs(adjList,visited,adj,ans);
             }
         }
-        return true;
+        ans[index--]=curr; //topological order storing the ans
+    }
+    public int[] findOrder(int numCourses, int[][] pre) {
+        index=numCourses-1; //to store from last as toplogical order
+        int ans[]=new int[numCourses]; //ans array
+        List<List<Integer>> adjList=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adjList.add(new ArrayList<>());
+        }
+        for(int i=0;i<pre.length;i++){
+            adjList.get(pre[i][1]).add(pre[i][0]); //adding edges
+        }
+        int color[]=new int[numCourses]; //color array to detect cycle
+        for(int i=0;i<numCourses;i++)
+        {
+            if(isCycle(adjList,color,i))
+            {
+                return new int[]{}; //if cycle found return empty arr
+            }
+        }
+        boolean visited[]=new boolean[numCourses]; //visited array
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i])
+            {
+                dfs(adjList,visited,i,ans); //simple dfs call
+            }
+        }
+        
+        return ans;
     }
 }
