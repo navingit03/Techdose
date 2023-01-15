7d7f8b4b9574f637d7d8b7eeefc5cd65558233b6
Navin Venkatesh K <112114128+navingit03@users.noreply.github.com>
2023-01-15 14:54:25 +0530

    Assignment

--- DynamicProgramming/LCS_LIS_LPS/FindLongestCommonSubsequence.java
+++ DynamicProgramming/LCS_LIS_LPS/FindLongestCommonSubsequence.java
@@ -1,8 +1,6 @@
 //using dp and finding the LCS
 public class LongestCommonSubsequence {
-    public static void main(String[] args) {
-        String text1="ABCDGH";
-        String text2="AEDFHR";
+    public static String findlcs(String text1,String text2){
         int len1=text1.length();
         int len2=text2.length();
         int dp[][]=new int[len1+1][len2+1];
@@ -23,13 +21,13 @@ public class LongestCommonSubsequence {
             }
         }
         //to find the lcs string
-        String ans=""; //ADH
+        String lcs=""; //ADH
         int i=len1,j=len2;
         while(i>0&&j>0) //repeat till valid index
         {
             if(text1.charAt(i-1)==text2.charAt(j-1)) //if both character are equal add them in ans (i-1,j-1 as dp is 1  indexed)
             {
-                ans=text1.charAt(i-1)+ans;
+                lcs=text1.charAt(i-1)+lcs;
                 i--; j--; //going to before diagonal
             }else //if not equal 
             {
@@ -43,6 +41,39 @@ public class LongestCommonSubsequence {
                 }
             }
         }
-        System.out.print(ans);
+        return lcs;
+    }
+    public static String findlcsStringDPtable(String text1,String text2){
+        int len1=text1.length();
+        int len2=text2.length();
+        String dp[][]=new String[len1+1][len2+1];
+        for(int i=0;i<=len1;i++){
+            for(int j=0;j<=len2;j++){
+                if(i==0||j==0) //if anyone is empty then return ""
+                {
+                    dp[i][j]="";
+                }
+                else if(text1.charAt(i-1)==text2.charAt(j-1)) //if i and j th char are same add char to dp[i-1][j-1](subproblem)
+                {
+                    dp[i][j]=dp[i-1][j-1]+text1.charAt(i-1);
+                }
+                else
+                {
+                    if(dp[i-1][j].length()>=dp[i][j-1].length()){
+                        dp[i][j]=dp[i-1][j];
+                    }else{
+                        dp[i][j]=dp[i][j-1];
+                    }
+                }
+            }
+        }
+        String lcs=dp[len1][len2];
+        return lcs;
+    }
+    public static void main(String[] args) {
+        String text1="ABCDGH";
+        String text2="AEDFHR";
+        System.out.println(findlcs(text1, text2)); //method 1 using dp table - ADH(ans)
+        System.out.println(findlcsStringDPtable(text1,text2)); //method 2 finding lcs in String [][]dp table - ADH(ans)
     }
 }
