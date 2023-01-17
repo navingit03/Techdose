6e1bdcf47a5259eb3813c54e94c8e4cbaa54102b
Navin Venkatesh K <112114128+navingit03@users.noreply.github.com>
2023-01-17 21:42:30 +0530

    Assignment

---
+++ TRIE/2185.CountingWordsWithAGivenPrefix.java
@@ -0,0 +1,42 @@
+class Solution {
+    TrieNode root;
+    class TrieNode
+    {
+        TrieNode next[]=new TrieNode[26];
+        int prefixctr;
+    }
+    public void insert(String s)
+    {
+        TrieNode temp=root;
+        for(int i=0;i<s.length();i++)
+        {
+            int index=s.charAt(i)-'a';
+            if(temp.next[index]==null)
+            {
+                temp.next[index]=new TrieNode();
+            }
+            temp=temp.next[index];
+            temp.prefixctr++;
+        }
+    }
+    public int prefixcount(String pre)
+    {
+        TrieNode temp=root;
+        for(int i=0;i<pre.length();i++)
+        {
+            int index=pre.charAt(i)-'a';
+            if(temp.next[index]==null) return 0;
+            temp=temp.next[index];
+        }
+        return temp.prefixctr;
+    }
+
+    public int prefixCount(String[] words, String pref) {
+        root=new TrieNode();
+        for(int i=0;i<words.length;i++)
+        {
+            insert(words[i]);
+        }
+        return prefixcount(pref);
+    }
+}
