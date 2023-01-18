b815d40cc41cc6f539436dd146119d21946f02d6
Navin Venkatesh K <112114128+navingit03@users.noreply.github.com>
2023-01-18 11:49:12 +0530

    Assignment

--- TRIE/139.WordBreak.java
+++ TRIE/139.WordBreak.java
@@ -23,23 +23,23 @@ class Solution {
     {
         Queue<Integer> q=new LinkedList<>();
         int len=s.length();
-        boolean visited[]=new boolean[len];
+        boolean visited[]=new boolean[len]; //to mark visited
         q.add(0); //starting index(base)
         outter:
         while(!q.isEmpty())
         {
-            int st=q.poll();
+            int st=q.poll(); //polling st index
             if(visited[st]==true) continue; //if already visited continue
-            visited[st]=true;
+            visited[st]=true; //make it visited
             TrieNode temp=root;
             for(int i=st;i<len;i++)
             {
                 int index=s.charAt(i)-'a';
                 temp=temp.next[index];
-                if(temp==null) continue outter;
-                if(temp.wordEnd>0&&i<len-1) q.add(i+1);
+                if(temp==null) continue outter; //if not found st from while
+                if(temp.wordEnd>0&&i<len-1) q.add(i+1); //check if word end here and add next index in queue
             }
-            if(temp.wordEnd>=1) return true;
+            if(temp.wordEnd>=1) return true; //if all substrings are present
         }
         return false;
     }
@@ -49,6 +49,6 @@ class Solution {
         {
             insert(wordDict.get(i));
         }
-        return find(s);
+        return find(s); //find if s is present in trie as substrings
     }
 }
