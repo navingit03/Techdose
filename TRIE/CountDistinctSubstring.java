751d8d529e5e4812f8ec8f3caad4fbab6645afa8
Navin Venkatesh K <112114128+navingit03@users.noreply.github.com>
2023-01-18 13:54:35 +0530

    Assignment

---
+++ TRIE/CountDistinctSubstring.java
@@ -0,0 +1,36 @@
+class countdistinctsubstring
+{
+    public static TrieNode root=null;
+    class TrieNode
+    {
+        TrieNode next[]=new TrieNode[26];
+    }
+    public static int result=0;
+    
+    public static void insert(String s)
+    {
+        TrieNode temp=root;
+        for(int i=0;i<s.length();i++)
+        {
+            int index=s.charAt(i)-'a';
+            if(temp.next[index]==null)
+            {
+                countdistinctsubstring obj=new countdistinctsubstring();
+                temp.next[index]=obj.new TrieNode();
+                result++;
+            }
+            temp=temp.next[index];
+        }
+    }
+
+    public static void main(String[] args) {
+        countdistinctsubstring obj=new countdistinctsubstring();
+        root=obj.new TrieNode();
+        String s="abc";
+        for(int i=0;i<s.length();i++)
+        {
+            insert(s.substring(i));
+        }
+        System.out.println("total distinct substring of s is "+result+1); //plus one for empty substring
+    }
+}
