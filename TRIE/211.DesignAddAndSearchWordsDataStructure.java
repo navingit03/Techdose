5747ee3aef850c13139e806b49358f02b794c04b
Navin Venkatesh K <112114128+navingit03@users.noreply.github.com>
2023-01-18 11:52:49 +0530

    Assignment

--- TRIE/211.DesignAddAndSearchWordsDataStructure.java
+++ TRIE/211.DesignAddAndSearchWordsDataStructure.java
@@ -10,7 +10,7 @@ class WordDictionary {
         root=new TrieNode();
     }
     
-    public void addWord(String word) {
+    public void addWord(String word) { //insert operation
         TrieNode temp=root;
         for(int i=0;i<word.length();i++)
         {
@@ -26,23 +26,23 @@ class WordDictionary {
     }
     public boolean dfs(String word,int index,TrieNode temp)
     {
-        if(index==word.length()) return temp.we>=1;
-        char ch=word.charAt(index);
-        if(ch=='.')
+        if(index==word.length()) return temp.we>=1; //if end is reached and there is a wordend
+        char ch=word.charAt(index); //curr char
+        if(ch=='.') //if '.' then check all possible node which is not null
         {
             for(int i=0;i<26;i++)
             {
-                if(temp.next[i]!=null && dfs(word,index+1,temp.next[i]))
+                if(temp.next[i]!=null && dfs(word,index+1,temp.next[i])) //dfs call of index+1
                 {
                     return true;
                 }
             }
         }
-        else if(temp.next[ch-'a']!=null)
+        else if(temp.next[ch-'a']!=null) //if a char is present at curr index and not null call dfs for index+1
         {
             return dfs(word,index+1,temp.next[ch-'a']);
         }
-        return false;
+        return false; //if not found
     }
     public boolean search(String word) {
         return dfs(word,0,root);
