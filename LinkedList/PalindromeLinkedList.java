dc8a55035031b75bb8abea30eebc988efb1903f1
Navin Venkatesh K <112114128+navingit03@users.noreply.github.com>
2023-02-25 12:33:44 +0530

    Assignment

--- LinkedList/PalindromeLinkedList.java
+++ LinkedList/PalindromeLinkedList.java
@@ -10,6 +10,51 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next==null) return true;
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode temp=null;
+        while(fast!=null && fast.next!=null)
+        {
+            temp=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode prev=temp;
+        ListNode curr=slow;
+        ListNode next=slow.next;
+        while(curr!=null)
+        {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        } 
+        while(head!=slow)
+        {
+            if(head.val!=prev.val)
+            {
+                return false;
+            }
+            head=head.next;
+            prev=prev.next;
+        }
+        return true;
+    }
+}
+//SC: O(n)
 class Solution {
     public boolean isPalindrome(ListNode head) {
         List<Integer> list=new ArrayList<>(); //creating a list to add elements
