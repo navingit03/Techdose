a37f320ef7221a3c3a03b948684435646be4786f
Navin Venkatesh K <112114128+navingit03@users.noreply.github.com>
2023-02-25 20:59:41 +0530

    Assignment

---
+++ DS_Implementation/StackUsingArray.java
@@ -0,0 +1,43 @@
+package BuildDS;
+public class StackUsingArray {
+    static final int MAX_SIZE=30;
+    static int s[]=new int[MAX_SIZE];
+    static int topOfStack=-1;
+    public static void push(int data)
+    {
+        topOfStack++;
+        if(topOfStack==MAX_SIZE)
+        {
+            System.out.println("Stack is Full");
+        }else{
+        s[topOfStack]=data;
+        }
+    }
+    public static int pop()
+    {
+        if(topOfStack<0)
+        {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return s[topOfStack--];
+        
+    }
+    public static int peek()
+    {
+        if(topOfStack<0)
+        {
+            System.out.println("Stack is Empty");
+            return -1;
+        }
+        return s[topOfStack];
+    }
+    public static void main(String[] args) {
+        push(5);
+        push(7);
+        push(8);
+        System.out.println( pop() );
+        System.out.println( peek() );
+        System.out.println( pop() );
+    }
+}
